cdf0d4e2d42501b4204b5938ab4c7011679bf67f
Mosra Rakesh <rakeshtech234@gmail.com>
2023-01-22 13:52:52 +0530

    first commit

---
+++ src/main/java/com/photoapp/api/users/accountmanagement/photoappusersaccountmanagementapi/PhotoappusersaccountmanagementapiApplication.java
@@ -0,0 +1,15 @@
+package com.photoapp.api.users.accountmanagement.photoappusersaccountmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PhotoappusersaccountmanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoappusersaccountmanagementapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/photoapp/api/users/accountmanagement/photoappusersaccountmanagementapi/PhotoappusersaccountmanagementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.api.users.accountmanagement.photoappusersaccountmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappusersaccountmanagementapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
